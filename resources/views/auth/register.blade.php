932186e218c790bb6658296f6a845a34c0dab9d3
yagiyuta <yagi0629yu@gmail.com>
2024-10-01 05:59:32 +0900

    register CSS完了

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -4,25 +4,48 @@
 <!-- 適切なURLを入力してください -->
 {!! Form::open(['url' => '/register']) !!}
 
-<h2>新規ユーザー登録</h2>
+<div class="login-form">
+  <div class="login-form-text">
+    <h2>新規ユーザー登録</h2>
+  </div>
 
-{{ Form::label('ユーザー名') }}
-{{ Form::text('username', null, ['class' => 'input']) }}
+  <div class="login-form-input">
+    <div class="input-form">
+      {{ Form::label('ユーザー名') }}
+      {{ Form::text('username', null, ['class' => 'input']) }}
+    </div>
 
-{{ Form::label('メールアドレス') }}
-{{ Form::text('mail', null, ['class' => 'input']) }}
+    <div class="input-form">
+      {{ Form::label('メールアドレス') }}
+      {{ Form::text('mail', null, ['class' => 'input']) }}
+    </div>
 
-{{ Form::label('パスワード') }}
-{{ Form::text('password', null, ['class' => 'input']) }}
+    <div class="input-form">
+      {{ Form::label('パスワード') }}
+      {{ Form::text('password', null, ['class' => 'input']) }}
+    </div>
 
-{{ Form::label('パスワード確認') }}
-{{ Form::text('password_confirmation', null, ['class' => 'input']) }}
+    <div class="input-form">
+      {{ Form::label('パスワード確認') }}
+      {{ Form::text('password_confirmation', null, ['class' => 'input']) }}
+    </div>
 
-{{ Form::submit('登録') }}
-
-<p><a href="/login">ログイン画面へ戻る</a></p>
-
-{!! Form::close() !!}
+    <div class="login-form-user-button">
+      {{ Form::submit('新規登録') }}
+    </div>
 
 
-@endsection
+
+  </div>
+
+
+  <div class="login-form-link">
+    <p><a href="/login">ログイン画面へ戻る</a></p>
+  </div>
+
+
+  {!! Form::close() !!}
+
+  @endsection
+
+</div>

e83ebe9de4db8f4554dd149c03942ef265f51e63
yagiyuta <yagi0629yu@gmail.com>
2024-10-20 14:59:34 +0900

    jsの処理にコメントアウトで説明を追記

--- public/js/script.js
+++ public/js/script.js
@@ -19,14 +19,18 @@ window.onclick = function (event) {
   }
 };
 
-const fileSelect = document.getElementById("fileSelect");
-const fileElem = document.getElementById("fileElem");
+const fileSelect = document.getElementById("fileSelect");//fileSelectをdocument.getElementById("fileSelect")と定義
+const fileElem = document.getElementById("fileElem");//fileElemをdocument.getElementById("fileElem")と定義
 
-fileSelect.addEventListener("click", (e) => {
+fileSelect.addEventListener("click", () => {
   if (fileElem) {
     fileElem.click();
   }
 }, false);
+//fileSelect.addEventListener("click",):ボタンがクリックされたときに何かをするように設定
+//() => { ... }:ボタンがクリックされたときに実行される動きの設定
+//if (fileElem) { ... }:ファイルを選ぶための隠れた部分が存在するか確認
+//つまりボタンを押した際に画面上にないファイル選択画面を自動で開く処理
 
 fileElem.addEventListener("change", function () {
   if (fileElem.files.length > 0) {
@@ -40,7 +44,9 @@ fileElem.addEventListener("change", function () {
       }
       return fileName;
     }).join(', ');
-
+    //fileElem.addEventListener("change", function () { ... }):fileElem（ファイル選択の <input>）の内容が変更された時（ファイルが選ばれた時）に実行される処理
+    //if (fileElem.files.length > 0)  ファイルが選ばれたかどうかの確認
+    //選んだファイル名が長すぎる際レイアウトを崩さないように名前を省略して...拡張子にする設定
     fileSelect.textContent = files;
   } else {
     fileSelect.textContent = 'ファイルを選択';
